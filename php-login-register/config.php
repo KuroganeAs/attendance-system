fcf3e449e76ccf2e7724b2af5d7ab32882da861c
Kuro <144928358+KuroganeAs@users.noreply.github.com>
2025-12-16 23:37:03 +0700

    Add files via upload

---
+++ php-login-register/config.php
@@ -0,0 +1,18 @@
+<?php
+// Pastikan hostnya "db", bukan "localhost"
+$host = "db";
+$dbname = "attendance_system";
+$user = "annisa";
+$pass = "12345";
+
+try {
+    $db = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("DB Connection failed: " . $e->getMessage());
+}
+?>
